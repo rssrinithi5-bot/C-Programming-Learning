b70aa1061858dd49423c0065bc26494cbcc2c363
rssrinithi <rssrinithi5@gmail.com>
2026-08-12 15:36:48 +0530

    Add student details program in C

---
+++ 01_Data_Types/student_details.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 19;
+    float cgpa = 8.7f;
+    char grade = 'A';
+
+    printf("Age = %d\n", age);
+    printf("CGPA = %.1f\n", cgpa);
+    printf("Grade = %c\n", grade);
+
+    return 0;
+}
